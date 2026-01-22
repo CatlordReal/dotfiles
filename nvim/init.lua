ad2e0f74bf8298c8086e3ef6f8ca7359887784fa
Kian <143936856+KitcatCatlord@users.noreply.github.com>
2026-01-22 20:32:18 +0000

    Switch from nvim-tree to neo-tree in nvim
    Has a cleaner UI

--- nvim/init.lua
+++ nvim/init.lua
@@ -213,93 +213,102 @@ require("lazy").setup({
         end,
     },
     -- File explorer
-    {
-        "nvim-tree/nvim-tree.lua",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("nvim-tree").setup({
-                -- Use on_attach to customise key mappings when the tree is focused
-                on_attach = function(bufnr)
-                    local api = require("nvim-tree.api")
-                    local function opts(desc)
-                        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-                    end
-                    -- load default mappings then add our own
-                    api.config.mappings.default_on_attach(bufnr)
-                    -- Open the selected file in a new tab without leaving the tree when pressing 't'
-                    vim.keymap.set("n", "t", function()
-                        local node = api.tree.get_node_under_cursor()
-                        if not node or node.type ~= "file" then
-                            return
-                        end
-                        -- add to buffer list
-                        vim.cmd("badd " .. vim.fn.fnameescape(node.absolute_path))
-                        -- close the tree window if desired
-                        api.tree.close()
-                    end, opts("Open in buffer, close tree"))
-                end,
-                view = {
-                    width = 32,
-                    side = "left",
-                    signcolumn = "yes",
-                },
 
-                renderer = {
-                    root_folder_label = false,
-                    highlight_git = true,
-                    highlight_opened_files = "name",
-                    indent_markers = {
-                        enable = true,
-                        icons = {
-                            corner = "└ ",
-                            edge = "│ ",
-                            item = "│ ",
-                            none = "  ",
-                        },
-                    },
-                    icons = {
-                        git_placement = "before",
-                        diagnostics_placement = "after",
-                        padding = " ",
-                        symlink_arrow = " ➛ ",
-                        show = {
-                            file = true,
-                            folder = true,
-                            folder_arrow = false,
-                            git = true,
-                            diagnostics = true,
-                        },
-                        glyphs = {
-                            default = "",
-                            symlink = "",
-                            bookmark = "",
-                            folder = {
-                                arrow_closed = "",
-                                arrow_open = "",
-                                default = "",
-                                open = "",
-                                empty = "",
-                                empty_open = "",
-                                symlink = "",
-                                symlink_open = "",
-                            },
-                            git = {
-                                unstaged = "✗",
-                                staged = "✓",
-                                unmerged = "",
-                                renamed = "➜",
-                                untracked = "★",
-                                deleted = "",
-                                ignored = "◌",
-                            },
-                        },
-                    },
-                },
-            })
-            -- Leader key mapping for toggling the tree
-            vim.keymap.set("n", "<leader>nt", "<cmd>NvimTreeToggle<CR>", { silent = true, desc = "Toggle Nvim Tree" })
-        end,
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "main",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
     },
+    -- {
+    --     "nvim-tree/nvim-tree.lua",
+    --     dependencies = { "nvim-tree/nvim-web-devicons" },
+    --     config = function()
+    --         require("nvim-tree").setup({
+    --             -- Use on_attach to customise key mappings when the tree is focused
+    --             on_attach = function(bufnr)
+    --                 local api = require("nvim-tree.api")
+    --                 local function opts(desc)
+    --                     return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    --                 end
+    --                 -- load default mappings then add our own
+    --                 api.config.mappings.default_on_attach(bufnr)
+    --                 -- Open the selected file in a new tab without leaving the tree when pressing 't'
+    --                 vim.keymap.set("n", "t", function()
+    --                     local node = api.tree.get_node_under_cursor()
+    --                     if not node or node.type ~= "file" then
+    --                         return
+    --                     end
+    --                     -- add to buffer list
+    --                     vim.cmd("badd " .. vim.fn.fnameescape(node.absolute_path))
+    --                     -- close the tree window if desired
+    --                     api.tree.close()
+    --                 end, opts("Open in buffer, close tree"))
+    --             end,
+    --             view = {
+    --                 width = 32,
+    --                 side = "left",
+    --                 signcolumn = "yes",
+    --             },
+
+    --             renderer = {
+    --                 root_folder_label = false,
+    --                 highlight_git = true,
+    --                 highlight_opened_files = "name",
+    --                 indent_markers = {
+    --                     enable = true,
+    --                     icons = {
+    --                         corner = "└ ",
+    --                         edge = "│ ",
+    --                         item = "│ ",
+    --                         none = "  ",
+    --                     },
+    --                 },
+    --                 icons = {
+    --                     git_placement = "before",
+    --                     diagnostics_placement = "after",
+    --                     padding = " ",
+    --                     symlink_arrow = " ➛ ",
+    --                     show = {
+    --                         file = true,
+    --                         folder = true,
+    --                         folder_arrow = false,
+    --                         git = true,
+    --                         diagnostics = true,
+    --                     },
+    --                     glyphs = {
+    --                         default = "",
+    --                         symlink = "",
+    --                         bookmark = "",
+    --                         folder = {
+    --                             arrow_closed = "",
+    --                             arrow_open = "",
+    --                             default = "",
+    --                             open = "",
+    --                             empty = "",
+    --                             empty_open = "",
+    --                             symlink = "",
+    --                             symlink_open = "",
+    --                         },
+    --                         git = {
+    --                             unstaged = "✗",
+    --                             staged = "✓",
+    --                             unmerged = "",
+    --                             renamed = "➜",
+    --                             untracked = "★",
+    --                             deleted = "",
+    --                             ignored = "◌",
+    --                         },
+    --                     },
+    --                 },
+    --             },
+    --         })
+    --         -- Leader key mapping for toggling the tree
+    --         vim.keymap.set("n", "<leader>nt", "<cmd>NvimTreeToggle<CR>", { silent = true, desc = "Toggle Nvim Tree" })
+    --     end,
+    -- },
     {
         "folke/flash.nvim",
         event = "VeryLazy",
@@ -987,7 +996,8 @@ wkr = require("which-key") -- alias for clarity in modifications
 wkr.add({
     -- File explorer
     { "<leader>e",  function() require("oil").open() end,                     desc = "File Explorer (Oil)" },
-    { "<leader>nt", "<cmd>NvimTreeToggle<CR>",                                desc = "Toggle Nvim Tree" },
+    -- { "<leader>nt", "<cmd>NvimTreeToggle<CR>",                                desc = "Toggle Nvim Tree" },
+    { "<leader>nt", "<cmd>Neotree toggle filesystem left<CR>",                desc = "Toggle Neo-tree" },
     -- Find
     { "<leader>f",  group = "Find" },
     { "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find Files" },
